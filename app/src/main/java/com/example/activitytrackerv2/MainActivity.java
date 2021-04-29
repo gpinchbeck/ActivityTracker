962392f309aa003e3a9c9ada4cac270ba857246e
Hammad Jehangir <55459823+hammadh19@users.noreply.github.com>
2021-04-29 14:22:12 +0100

    Add files via upload

--- app/src/main/java/com/example/activitytrackerv2/MainActivity.java
+++ app/src/main/java/com/example/activitytrackerv2/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +29,66 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
+    }
+
+    public void browser1(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=MxnzcssW-tk"));
+        startActivity(browserIntent);
+    }
+
+    public void browser2(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=2Vprklw8cu8"));
+        startActivity(browserIntent);
+    }
+
+    public void browser3(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=8LJ3Q3Fsrzs"));
+        startActivity(browserIntent);
+    }
+
+    public void browser4(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=Xg9B6pqHUQE&t=320s"));
+        startActivity(browserIntent);
+    }
+
+    public void browser5(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=Yz8iZCsG2_U"));
+        startActivity(browserIntent);
+    }
+
+    public void browser6(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=zqDSELw3IA0"));
+        startActivity(browserIntent);
+    }
+
+    public void diet1(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=0BZxQYXLDbQ"));
+        startActivity(browserIntent);
+    }
+
+    public void diet2(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=P_wD2zydD_g"));
+        startActivity(browserIntent);
+    }
+
+    public void diet3(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=ishwT92D8Ec"));
+        startActivity(browserIntent);
+    }
+
+    public void diet4(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=wJe71nnjLmw"));
+        startActivity(browserIntent);
+    }
+
+    public void diet5(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=LCyECbA3pUw&t=464s"));
+        startActivity(browserIntent);
+    }
+
+    public void diet6(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=5s10IzwlXfE"));
+        startActivity(browserIntent);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
